6556cd83f4bdd8f56a0082a90668fdde9f396b76
code3743 <jota.lopez@correounivalle.edu.co>
2024-10-07 11:34:09 -0500

    Add print statements for algorithm tests and initialize algorithm module

--- test/smart_terminal_test.py
+++ test/smart_terminal_test.py
@@ -9,6 +9,7 @@ from smart_terminal.utils.apply_operations import apply_operations
 class SmartTerminalTest(unittest.TestCase):
     
     def test_algorithm_dynamic_programming(self):
+        print("\nTesting dynamic programming algorithm...")
         cost = {'advance': 1, 'delete': 2, 'replace': 3, 'insert': 2, 'kill': 1}
         base_string = "algorithm"
         target_string = "altruistic"
@@ -28,10 +29,12 @@ class SmartTerminalTest(unittest.TestCase):
         final_cost, steps = transform_string_dp(base_string, target_string, cost)
         self.assertEqual(final_cost, expected_transformation_cost, 'The cost is not correct.')
         self.assertEqual(target_string, apply_operations(base_string, steps), 'The transformation is not correct.')
+        
 
 
 
     def test_algorithm_greedy(self):
+        print("\nTesting greedy algorithm...")
         cost = {'advance': 1, 'delete': 2, 'replace': 3, 'insert': 2, 'kill': 1}
         base_string = "algorithm"
         target_string = "altruistic"
@@ -47,6 +50,7 @@ class SmartTerminalTest(unittest.TestCase):
         self.assertEqual(target_string, apply_operations(base_string, steps), 'The transformation is not correct.')
 
     def test_algorithm_brute_force(self):
+        print("\nTesting brute force algorithm...")
         cost = {'advance': 1, 'delete': 2, 'replace': 3, 'insert': 2, 'kill': 1}
         base_string = "algorithm"
         target_string = "altruistic"
