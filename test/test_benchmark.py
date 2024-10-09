466f5f096122472ccd610e86747213bbc801f0a4
code3743 <jota.lopez@correounivalle.edu.co>
2024-10-08 21:39:47 -0500

    Enhance benchmarking class with theoretical cost tracking

--- test/test_benchmark.py
+++ test/test_benchmark.py
@@ -20,40 +20,36 @@ class Benchmark:
         Number of iterations to run each function for benchmarking.
     results : dict
         Dictionary to store the benchmarking results.
-    Methods:
-    --------
-    add_function(func, case):
-        Adds a function and its test case to the benchmark and runs the benchmark.
-    _benchmark_function(func, *args):
-        Benchmarks a single function over the specified number of iterations.
-    plot_results_per_case(save_folder='results', log_scale=True):
-        Plots the execution time of each function for each test case and saves the plots.
-    plot_average_results(save_folder='results', log_scale=True):
-        Plots the average execution time of each function across all test cases and saves the plot.
+    theoretical_costs : dict
+        Dictionary to store the theoretical costs for each function and case.
     """
-    def __init__(self, num_iterations=50):
+    def __init__(self, num_iterations=50, save_folder='results', prefix=''):
         self.num_iterations = num_iterations
         self.results = {}
+        self.save_folder = save_folder
+        self.prefix = prefix
+        self.theoretical_costs = {}
 
-    def add_function(self, func, case):
-        
+    def add_function(self, func, case, theoretical_complexity):
         case_name = case['name']
         if case_name not in self.results:
             self.results[case_name] = {}
+            self.theoretical_costs[case_name] = {}
 
         func_name = func.__name__
         self.results[case_name][func_name] = self._benchmark_function(func, *case['args'])
+        self.theoretical_costs[case_name][func_name] = theoretical_complexity(case)
 
     def _benchmark_function(self, func, *args):
         times = []
         for _ in range(self.num_iterations):
-            start_time = time.time()  
-            func(*args) 
-            end_time = time.time() 
-            times.append(end_time - start_time) 
+            start_time = time.time()
+            func(*args)
+            end_time = time.time()
+            times.append(end_time - start_time)
         return np.array(times)
 
-    def plot_results_per_case(self, save_folder='results', log_scale=True):
+    def plot_results_per_case(self, log_scale=True):
         for case_name, funcs_times in self.results.items():
             plt.figure(figsize=(10, 6))
             for func_name, times in funcs_times.items():
@@ -68,18 +64,15 @@ class Benchmark:
             if log_scale:
                 plt.yscale('log')
 
-            if save_folder:
-                if not os.path.exists(save_folder):
-                    os.makedirs(save_folder)
+            if self.save_folder:
+                if not os.path.exists(self.save_folder):
+                    os.makedirs(self.save_folder)
 
-                file_path = os.path.join(save_folder, case_name.replace(' ', '_').lower() + '.png')
+                file_path = os.path.join(self.save_folder, self.prefix + case_name.replace(' ', '_').lower() + '.png')
                 plt.savefig(file_path)
                 print(f'Graph saved in: {file_path}')
 
-            plt.show()
-
-    def plot_average_results(self, save_folder='results', log_scale=True):
-
+    def plot_average_results(self, log_scale=True):
         average_times = {}
 
         for _, funcs in self.results.items():
@@ -94,7 +87,7 @@ class Benchmark:
 
         for func_name, times in average_times.items():
             plt.plot(times, label=f'{func_name} (mean: {overall_averages[func_name]:.6f} s)')
-
+      
         plt.title('Average Execution Time per Function Across All Cases')
         plt.xlabel('Case Number')
         plt.ylabel('Average Time (s)')
@@ -105,42 +98,85 @@ class Benchmark:
         if log_scale:
             plt.yscale('log')
 
-        if save_folder:
-            if not os.path.exists(save_folder):
-                os.makedirs(save_folder)
+        if self.save_folder:
+            if not os.path.exists(self.save_folder):
+                os.makedirs(self.save_folder)
 
-            file_path = os.path.join(save_folder, 'average_results.png')
+            file_path = os.path.join(self.save_folder, self.prefix +  'average_results.png')
             plt.savefig(file_path)
             print(f'Average graph saved in: {file_path}')
 
+    def plot_theoretical(self, log_scale=True):
+        plt.figure(figsize=(10, 6))
+
+        case_names = list(self.theoretical_costs.keys())
+        theoretical_values = {func_name: [] for func_name in next(iter(self.theoretical_costs.values())).keys()}
+        
+        for case_name in case_names:
+            for func_name, cost in self.theoretical_costs[case_name].items():
+                theoretical_values[func_name].append(cost)
+
+        for func_name, costs in theoretical_values.items():
+            plt.plot(case_names, costs, marker='o', label=f'{func_name} (theoretical)')
+
+            for i, cost in enumerate(costs):
+                plt.annotate(f'{cost}', 
+                            (case_names[i], cost), 
+                            textcoords="offset points", 
+                            xytext=(0, 5), 
+                            ha='center') 
+
+        plt.title('Theoretical Costs Across Cases')
+        plt.xlabel('Case Number')
+        plt.ylabel('Theoretical Cost')
+        plt.xticks(ticks=range(len(case_names)), labels=[f'Case {i + 1}' for i in range(len(case_names))])
+        plt.legend()
+        plt.grid(True)
+
+        
+        if log_scale:
+            plt.yscale('log')
+            plt.gca().yaxis.set_visible(False)
+
+        if self.save_folder:
+            if not os.path.exists(self.save_folder):
+                os.makedirs(self.save_folder)
+
+            file_path = os.path.join(self.save_folder, self.prefix + 'theoretical.png')
+            plt.savefig(file_path)
+            print(f'Theoretical graph saved in: {file_path}')
 
 
 if __name__ == '__main__':
-    benchmark = Benchmark()
+    benchmark = Benchmark(num_iterations=50, prefix='smart_terminal_')
     cost = {'advance': 1, 'delete': 2, 'replace': 3, 'insert': 2, 'kill': 1}
     A = 1000
     B = 100
 
     string_transform_cases = [
         {
+            'name': 'Case a to ab',
+            'args': ['a', 'ab', cost],
+        },
+        {
+            'name': 'Case hola to chao',
+            'args': ['bc', 'ab', cost]
+        },
+        {
             'name': 'Case add to ada',
             'args': ['add', 'ada', cost]
         },
         {
-            'name': 'Case hola to chao',
-            'args': ['hola', 'chao', cost]
+            'name': 'Case hola to holi',
+            'args': ['hola', 'holi', cost]
         },
         {
-            'name': 'Case francesa to ancestro',
-            'args': ['francesa', 'ancestro', cost]
+            'name': 'Case perro to gatos',
+            'args': ['perro', 'gatos', cost]
         },
         {
             'name': 'Case ingenioso to ingeniero',
             'args': ['ingenioso', 'ingeniero', cost]
-        },
-        {
-            'name': 'Case algorithm to altruistic',
-            'args': ['algorithm', 'altruistic', cost]
         }
     ]
 
@@ -177,11 +213,19 @@ if __name__ == '__main__':
         }
     ]
 
+    '''
+    #TODO: Define theoretical complexity for each function
     for case in public_auction_cases:
         benchmark.add_function(auction_dp, case)
         benchmark.add_function(auction_brute_force, case)
         benchmark.add_function(auction_greedy, case)
+    '''
+
+    for case in string_transform_cases:
+        benchmark.add_function(transform_string_dp, case, lambda case: len(case['args'][0]) * len(case['args'][1]))
+        benchmark.add_function(transform_string_brute_force, case, lambda case: 4 ** (len(case['args'][0]) + len(case['args'][1])))
+        benchmark.add_function(transform_string_greedy, case, lambda case: len(case['args'][0]) + len(case['args'][1]))
 
     benchmark.plot_results_per_case()
-
     benchmark.plot_average_results()
+    benchmark.plot_theoretical()
