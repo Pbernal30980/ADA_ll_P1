9bceedb64c7e788c18b888e2154d44602699cb80
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2024-10-22 23:26:48 -0500

    Allow execution in main

--- app.py
+++ app.py
@@ -84,11 +84,7 @@ def transform_string():
 
     return jsonify({'total_cost': total_cost, 'steps': steps})
 
-
-
 def open_browser():
     webbrowser.open_new('http://127.0.0.1:5000/')
 
-if __name__ == '__main__':
-    open_browser() 
-    app.run(debug=True)
+
--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import tkinter as tk
-import public_auction.algorithm as pa
-import smart_terminal.algorithm as st
+from app import app
+from app import open_browser
 
-ventana_principal = tk.Tk()
-ventana_principal.title("Terminal Inteligente y Subasta Pública")
-
-tk.Label(ventana_principal, text="Elige qué interfaz deseas ejecutar:").pack(pady=10)
-
-btn_terminal = tk.Button(ventana_principal, text="Terminal Inteligente", command=st.abrir_terminal_inteligente)
-btn_terminal.pack(pady=5)
-
-btn_subasta = tk.Button(ventana_principal, text="Subasta Pública", command=pa.abrir_subasta)
-btn_subasta.pack(pady=5)
-
-ventana_principal.mainloop()
+open_browser() 
+app.run(debug=True)
